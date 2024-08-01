b88a31afe062182657b6c4007da7fc884c9edf9d
Dhanraj Kushwah <dhanraj.kushwah007@gmail.com>
2024-08-01 08:51:58 +0530

    Dhanraj kus

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = require('./routes');
-const cors = require('cors');
+//const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,12 +8,12 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
-app.use(cors({
-    origin:["https://deploy-mern-1whq.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
+// app.use(cors({
+//     origin:["https://deploy-mern-1whq.vercel.app"],
+//     methods:["POST","GET"],
+//     credentials:true
 
-}));
+// }));
 const mongoUri = process.env.MONGODB_URI='mongodb+srv://Contas:nuouP4MyDhH0q3E4@cluster0.gkkofhc.mongodb.net/Database';
 if (!mongoUri) {
     console.error('MONGODB_URI environment variable is not set.');
